21462dfce83af370878a0c39b40b255f83f8bdec
Dylan Santiago <78610577+notfennecks@users.noreply.github.com>
2023-11-10 03:08:03 -0500

    Stuff

--- GodotGameFiles/Global.gd
+++ GodotGameFiles/Global.gd
@@ -6,6 +6,12 @@ var current_class = all_classes[0]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Global script working")
+	var s = Summator.new()
+	s.add(10)
+	s.add(20)
+	s.add(30)
+	print(s.get_total())
+	s.reset()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
